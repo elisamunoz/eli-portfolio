df996eaa7fd41e7d0481853c511bb1edf0e25215
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2020-10-18 18:35:04 +0100

    used section component in home section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import classnames from 'classnames';
+import Section from'ui/components/Section'
 import Button from 'ui/components/Button';
 import './Home.scss';
 
 const Home = () => (
-  <section id='home' className='home vertical-center'>
-    <div className='container'>
-      <h1 className='title'>
-        Hi there, I'm Elisa!
-      </h1>
-      <h3 className='subtitle'>
-        I’m a Junior Software Developer looking for my first job in IT. I have just graduated from the Code Institute in Ireland.
-      </h3>
-      <Button className='btn'>
-        Download my Resume
-      </Button>
-    </div>
-  </section>
+  <Section
+      id='home'
+      className='home'
+      title="Hi there, I'm Elisa!"
+      subtitle="I’m a Junior Software Developer looking for my first job in IT. I have just graduated from the Code Institute in Ireland."
+  >
+    <Button>
+      Download my CV
+    </Button>
+  </Section>
 );
 
 export default Home;
