fb4e3f02d43f35e882e41fa3382ce0e4d731fb42
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2020-10-20 00:03:18 +0100

    remove duplicated sidebar

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Layout from 'ui/layout/index';
 import 'assets/styles/index.scss';
 import Home from './home';
 import Portfolio from './portfolio';
@@ -7,12 +6,12 @@ import About from './about'
 import Contact from './contact';
 
 const Page = () => (
-  <Layout>
+  <>
     <Home />
     <Portfolio />
     <About />
     <Contact />
-  </Layout>
+  </>
 );
 
 export default Page;
