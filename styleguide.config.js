4b0d3cdcb3e12c61e5d0dac7c6ae49b9732f5aff
Aitor <aitorpalomares@hotmail.com>
2020-10-21 23:50:04 +0100

    first styleguidist

---
+++ styleguide.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  skipComponentsWithoutExample: true,
+  sections: [
+    {
+      name: 'Documentation',
+      sections: [
+        {
+          name: 'Installation',
+          description: 'The description for the installation section'
+        },
+        {
+          name: 'Live Demo',
+          external: true,
+          href: 'http://example.com'
+        }
+      ]
+    },
+    {
+      name: 'UI Components',
+      components: 'src/ui/components/**/[A-Z]*.js',
+      exampleMode: 'expand', // 'hide' | 'collapse' | 'expand'
+      usageMode: 'expand' // 'hide' | 'collapse' | 'expand'
+    }
+  ]
+};
