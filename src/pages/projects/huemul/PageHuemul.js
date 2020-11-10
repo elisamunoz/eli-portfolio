4b00534c8e10d2ac866f136231ebc3a491aa1232
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2020-11-10 01:11:42 +0000

    deleted duplicated lines

--- src/pages/projects/huemul/PageHuemul.js
+++ src/pages/projects/huemul/PageHuemul.js
@@ -30,8 +30,6 @@ const Page = () => (
 
       <List title='Deployment:'>
         <ListItem>This website is hosted and deployed using GitHub pages.</ListItem>
-        <ListItem>Link to the GitHub repository <A target='_blank' href='https://github.com/elisamunoz/the-huemul-organization'>here</A></ListItem>
-        <ListItem>Link to the live demo <A target='_blank' href='https://elisamunoz.github.io/the-huemul-organization/'>here</A></ListItem>
         <ListItem>Link to the GitHub repository <LinkText href='https://github.com/elisamunoz/the-huemul-organization'>here</LinkText></ListItem>
         <ListItem>Link to the live demo <LinkText href='https://elisamunoz.github.io/the-huemul-organization/'>here</LinkText></ListItem>
       </List>
