4e207f11cfe27d38494dcdd9d70d84a2fec49c5e
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2020-11-10 00:37:48 +0000

    uploaded CV, added link to button for downloading

--- src/pages/home/home/Home.js
+++ src/pages/home/home/Home.js
@@ -3,7 +3,9 @@ import { HOME_SECTIONS } from 'const';
 import Button from 'ui/components/Button';
 import styles from './Home.module.scss';
 import homeImg from 'assets/img/home.jpg';
+import CV from 'assets/ElisaCV.pdf'
 import Landpage from 'ui/components/Landpage';
+import A from 'ui/components/A';
 
 const Home = () => (
   <Landpage
@@ -19,9 +21,14 @@ const Home = () => (
     img={homeImg}
     imgSection={styles.imgHome}
   >
-    <Button className={styles.btn}>
-      Download my CV
-    </Button>
+  <A
+    href={CV}
+    target='_blank'
+  >
+      <Button className={styles.btn}>
+        Download my CV
+      </Button>
+  </A>
   </Landpage>
   
 
