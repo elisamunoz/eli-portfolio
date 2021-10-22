237ea622d21a88a9db525a6907972c6afbe88066
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2021-10-22 15:47:33 +0200

    udate content

--- src/pages/home/about/About.js
+++ src/pages/home/about/About.js
@@ -24,7 +24,7 @@ const About = () => (
         <p>I am a creative and think outside the box kind of person, I love keeping myself busy meeting friends and with my hobbies which are knitting, crocheting, sewing, gardening, hiking and baking, I also enjoy learning new skills that help me to improve my professional and/or personal life. 
           <br />
           <br />    
-          I am originally from Chile, one day I decided to leave everything behind and to start my journey. I lived in Canada, Australia, Ireland and finally settled down in Spain. In Ireland was where studied and discovered the programming world and fell in love with it. I am actively looking for a job where I can use the new knowledge achieved during the past years. If you are curious about my personal projects visit my GitHub page <LinkText href='https://github.com/elisamunoz'>here</LinkText>.
+          I am originally from Chile, one day I decided to leave everything behind and to start my journey. I lived in Canada, Australia, Ireland and finally settled down in Spain. In Ireland was where I studied and discovered the programming world and fell in love with it. I am actively looking for a job where I can use the new knowledge achieved during the past years. If you are curious about my personal projects visit my GitHub page <LinkText href='https://github.com/elisamunoz'>here</LinkText>.
         </p>
       </div>
     </div>
