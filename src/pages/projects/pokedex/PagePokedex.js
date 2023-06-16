4af66cdf29049d1d25a992a07bc279e2654c02c7
elisamunoz <elisamunozes@gmail.com>
2023-06-16 12:19:32 +0200

    fixed figma link

--- src/pages/projects/pokedex/PagePokedex.js
+++ src/pages/projects/pokedex/PagePokedex.js
@@ -17,10 +17,11 @@ const Page = () => {
         img={Pic2}
       >
         <P>
+          When I was a child I used to watch Pokemon cartoons, then a couple of weeks ago I was surfing the web and I got lucky and came across this <LinkText href="https://www.figma.com/file/FrM5KLtKlSHa2jvDDGWwQt/Pok%C3%A9dex-(Community)?type=design&node-id=0-1&t=sk5nRptZx5dTbyYE-0" caption="Pokédex Prototype" /> in Figma, which is based on the <LinkText href="https://pokeapi.co/" caption="Poké API" /> and I thought it was going to be a lot of fun to create my own Pokédex and it was indeed!
+          <Break />
           This app gets information about the first Pokemon generation. It shows the 150 Pokemon in a list where you can chose any of them and see the Pokemon chosen in detail.
           <Break />
           This information is obtained from the <LinkText href="https://pokeapi.co/" caption="Poké API" /> which is a public API that contains all the Pokemon information you could imagine, including stats, description, image, name, etc.
-          When I was a child I used to watch Pokemon cartoons, then a couple of weeks ago I was surfing the web and I came across this <LinkText href="https://www.figma.com/file/m2quavvRgb9W2Q1uCkMYFE/Windows-95-Design-System-(Community)?node-id=70%3A17" caption="Pokédex Prototype" /> in Figma, which is based on the <LinkText href="https://pokeapi.co/" caption="Poké API" /> and I thought it was going to be a lot of fun to create my own Pokédex and it was indeed!
           <Break />
           Now I am happy and proud to show it to you.
         </P>
